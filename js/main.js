341b16356cb1712fbd5774f6be9db3c448245c93
SOHEE <118507151+devheee@users.noreply.github.com>
2022-12-28 17:13:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,11 +9,18 @@ $(function () {
         $('.num strong').text(s.slideCount)
     });
 
-    $('.aboutlg .title>li').on('click', function (event) {
+    $('.mainContent .title>li').on('click', function (event) {
         event.preventDefault();
         var _this = $(this);
         var idx = $(this).index();
         $(this).addClass('on').siblings().removeClass('on');
-        $('.aboutlg .contents_wrap').eq(idx).addClass('on').siblings().removeClass('on')
+        $('.mainContent .contents_wrap').eq(idx).addClass('on').siblings().removeClass('on')
     })
+
+    // $('.newsslide').slick({
+    //     arrows: false,
+    //     autoplay: false,
+    //     slidesToShow: 8,
+    //     slidesToScroll: 8,
+    // });
 })
